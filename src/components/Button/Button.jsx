1bb7b4ba714d8fe805fb0db5e3a6ae5cb55eba12
Alam <alampasha1103@gmail.com>
2024-05-13 19:06:27 +0530

    pushed code

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { MdMessage } from "react-icons/md";
+import styles from "./Button.module.css"
+
+const Button = ({isOutline, icon, text, ...rest}) => {
+
+  // const {isOutline, icon, text} = props; or directly pass in function
+
+  return (
+   <button {...rest} className={isOutline ?styles.outline_btn : styles.primary_btn}>
+    {icon}
+    {text}
+   </button>
+  )
+}
+
+export default Button
